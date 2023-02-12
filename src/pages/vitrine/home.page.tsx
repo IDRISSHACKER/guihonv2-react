f269c1f5d97fe58cb89673c1c15d325e546af7e0
IDR6IX DUVAL <idrissleutch@cinaf.tv>
2023-02-12 23:01:29 +0100

    v2

--- src/pages/vitrine/home.page.tsx
+++ src/pages/vitrine/home.page.tsx
@@ -4,6 +4,7 @@ export default ()=> {
         <div>
             <h1>Home Page</h1>
             <p>Ceci est une page de test deployer par CIRCLECI</p>
+            <b>Managed by <i>IIDRISS HACKER</i></b>
         </div>
     )
 }
